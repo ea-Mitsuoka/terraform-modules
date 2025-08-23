a8e68b278bbf8f6ff1f6895fe4f9582cbe41ed7f
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-08-24 00:09:27 +0900

    chore: add version

--- string_utils/versions.tf
+++ string_utils/versions.tf
@@ -1,4 +1,11 @@
 terraform {
   # "~>" を使い、意図しないメジャー/マイナーアップデートを防ぎます
   required_version = "~> 1.12.2"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.48.0"
+    }
+  }
 }
