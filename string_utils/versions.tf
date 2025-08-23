ae06918fb2c3c2f87bf5d4ac24741194a792841d
ea-Mitsuoka <yukihide.mitsuoka@dragon.jp>
2025-08-23 23:54:16 +0900

    chore: add version specification file

---
+++ string_utils/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  # "~>" を使い、意図しないメジャー/マイナーアップデートを防ぎます
+  required_version = "~> 1.12.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.48.0"
+    }
+  }
+}
